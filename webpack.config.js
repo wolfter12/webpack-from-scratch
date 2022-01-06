a49b11432a1df71cfa3701a268074cd36cb95237
Nazar <wolfter12@gmail.com>
2022-01-06 14:21:13 +0200

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,10 @@ module.exports = {
         },
       },
     ],
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html',
-      }),
-    ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
 };
